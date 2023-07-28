4efd202c697b68036bece3e8a96fa77dbce961ae
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-07-28 12:57:35 +0500

    main

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from tinydb import TinyDB
-
-db = TinyDB('data.json')
+import json
+from api import base_url
+import requests
+db = TinyDB('data.json',indent=4)
+for i in range(10):
+    response= requests.get(base_url)
+    r=response.json()
+    result=r["results"][-1]
+    user={"first_name":result["name"]["first"],
+        "last_name": result["name"]["last"],
+        "age": result["registered"]["age"],
+        "phone": result["phone"],
+        "country": result["location"]["country"],
+        "email": result["email"]
+        }
+    db.insert(user)
+    print(i)
+db.truncate()
